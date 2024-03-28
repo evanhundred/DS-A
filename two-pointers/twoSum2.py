3923e85eb33ff427e58423b1c7f5c62b3ac90622
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-03-28 11:28:08 -0400

    Handle lc edge - wip

--- two-pointers/twoSum2.py
+++ two-pointers/twoSum2.py
@@ -1,23 +1,19 @@
 def twoSum(numbers, target)
-    x = 0
-    y = 1
-    result = []
-    isComplete = False
-    while not isComplete:
-        if y == len(numbers):
-            if x == len(numbers) - 2:
-                isComplete = true
+        x = 0
+        y = len(numbers) - 1
+        result = []
+        isComplete = False
+        while not isComplete:
+            if y <= x:
+                if x == 
+                isComplete = True
                 break
+            num1 = numbers[x]
+            num2 = numbers[y]
+            if num1 + num2 == target:
+                result = [x + 1, y + 1]
+                isComplete = True
             else:
-                x += 1
-                y = x + 1
-                continue
-        num1 = numbers[x]
-        num2 = numbers[y]
-        elif x + y = target:
-            result = [num1, num2]
-            isComplete = True
-        else:
-            y += 1
+                y += 1
         
-    return result
+        return result
