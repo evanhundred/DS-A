70fbfcc14a2d93e11045c9fb64122b31c8810a55
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-03-28 10:51:38 -0400

    Update twoSum2.py

---
+++ two-pointers/twoSum2.py
@@ -0,0 +1,14 @@
+def twoSum(numbers, target)
+    x = 0
+    y = 1
+    result = []
+    isSuccess = False
+    while not isComplete:
+        if y == len(numbers):
+            
+        if x + y = target:
+            result = [x, y]
+        else:
+            y += 1
+        
+    return result
