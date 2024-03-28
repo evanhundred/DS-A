d4ef1c484dba32def578169850c3ef13cbfbf508
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-03-28 11:47:16 -0400

    Closer

--- two-pointers/twoSum2.py
+++ two-pointers/twoSum2.py
@@ -5,23 +5,23 @@ def twoSum(numbers, target)
         num1 = numbers[x]
         num2 = numbers[y]
         if num2 + num1 < target:
-                return result
+            return result
                 
-        lastItered = "x"
         isComplete = False
         while not isComplete:
             if y <= x:
-                isComplete = True
-                break
+                if x + y < target or x >= len(numbers) - 2:
+                    isComplete = True
+                    break
+                x += 1
+                y = len(numbers - 1)
+            num1 = numbers[x]
+            num2 = numbers[y]
             if num1 + num2 == target:
                 result = [x + 1, y + 1]
                 isComplete = True
+                break
             else:
-                if lastItered == "x":
-                    y += 1
-                    lastItered = "y"
-                else: 
-                    x += 1
-                    lastItered = "x"
+                y -= 1
         
         return result
